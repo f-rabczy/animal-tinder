6e63b2eb666e6ce6d717088469b63645e9d7d8df
Filip Rabczyński <filip@MacBook-Pro-Filip.local>
2022-12-08 23:56:44 +0100

    Initial commit

---
+++ src/main/java/pl/wsiz/animaltinder/AnimalTinderApplication.java
@@ -0,0 +1,13 @@
+package pl.wsiz.animaltinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalTinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalTinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wsiz/animaltinder/AnimalTinderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wsiz.animaltinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalTinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
