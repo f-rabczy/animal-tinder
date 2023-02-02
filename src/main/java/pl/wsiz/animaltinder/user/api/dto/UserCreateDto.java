23bbbc8202d0f89c4de68dd06dda8f33b5b558ae
f-rabczyn <f.rabczynski@gmail.com>
2023-02-02 20:21:36 +0100

    wylaczenie walidacji na usert create

--- src/main/java/pl/wsiz/animaltinder/user/api/dto/UserCreateDto.java
+++ src/main/java/pl/wsiz/animaltinder/user/api/dto/UserCreateDto.java
@@ -7,20 +7,12 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class UserCreateDto {
 
-    @NotEmpty(message = "Username must not be empty")
     private String username;
-    @NotEmpty(message = "Password must not be empty")
     private String password;
-    @NotEmpty(message = "Email must not be empty")
     private String email;
-    @NotEmpty(message = "First name must not be empty")
     private String firstName;
-    @NotEmpty(message = "Last name must not be empty")
     private String lastName;
-    @NotEmpty(message = "Last name must not be empty")
     private String city;
-    @NotEmpty(message = "Last name must not be empty")
     private String county;
-    @NotEmpty(message = "Last name must not be empty")
     private String voivodeship;
 }
